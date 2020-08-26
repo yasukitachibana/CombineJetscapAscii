8e086264153a41964995a684e91d839afeaca3e0
Yasuki Tachibana <yasukitachibana@Yasuki-MacBook-Pro.local>
2020-08-27 00:03:05 +0900

    latest

--- move_jetscape_ascii.py
+++ move_jetscape_ascii.py
@@ -5,8 +5,12 @@ import re
 import numpy as np
 import glob
 import shutil
+import generate_qsub_command as qcom
 
 def Move(args):
+    
+    home = os.getcwd()
+    os.chdir(args.target_directory)
     
     print('\n### Filename Starting with '+args.head_hadronlist)
     cwd = os.getcwd()
@@ -24,8 +28,14 @@ def Move(args):
         i_file = i_file + 1
         print(i_file,'/',N_file)
 
-        shutil.copyfile(file,os.path.join(args.destination,file))
-        command = 'cp '+os.path.join(cwd,file)+' '+os.path.join(args.destination,file)
+        #shutil.copyfile(file,os.path.join(args.destination,file))
+        command = '"cp '+os.path.join(cwd,file)+' '+os.path.join(args.destination,file)+'"'
+        master_command = os.path.join(home,'JobMaster') + ' "" ' + command
+        qsub_command = qcom.GenerateQsubCommand(string(i_file),master_command)
+        print('Submission: ')
+        print(qsub_command)
+        os.system(qsub_command)
+        print('###')
 
     filename = args.head_sigma+'*.*'
     excl_filename = args.head_sigma+'*'+args.suffix+'*.*'
@@ -33,9 +43,17 @@ def Move(args):
     list = set(glob.glob(filename)) - set(glob.glob(excl_filename))
     print(list)
 
-    for file in list:
-        shutil.copyfile(file,os.path.join(args.destination,file))
-        
+    for file in list:
+        command = '"cp '+os.path.join(cwd,file)+' '+os.path.join(args.destination,file)+'"'
+        master_command = os.path.join(home,'JobMaster') + ' "" ' + command
+        qsub_command = qcom.GenerateQsubCommand(string(i_file),master_command)
+        print('Submission: ')
+        print(qsub_command)
+        os.system(qsub_command)
+        print('###')
+
+    
+    os.chdir(home)
 
 def main():
 
@@ -48,11 +66,9 @@ def main():
     parser.add_argument("--head_hadronlist", type=str, default="JetscapeHadronListBin")
     parser.add_argument("--head_sigma", type=str, default="SigmaHardBin")
     args = parser.parse_args()
-    
-    home = os.getcwd()
-    os.chdir(args.target_directory)
+
     Move(args)
-    os.chdir(home)
+
 
 
 
